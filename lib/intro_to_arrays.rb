a22a9a5feb65489566b32b157c61d04fbc88661d
fernando <fernando_lopezc@live.com>
2019-02-08 13:34:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,15 +7,15 @@ def array_with_two_elements
 end
 
 def first_element
-  
+
 end
 
 def third_element
-  
+
 end
 
 def last_element
-  
+
 end
 
 def first_element_with_array_methods
@@ -27,5 +27,5 @@ def last_element_with_array_methods(arr)
   end
 
 def length_of_array(arr)
-arr.length   
+arr.length
 end
