7413845e853bea9324be10346df4a95ac7440c8d
fernando <fernando_lopezc@live.com>
2019-02-08 13:34:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,27 @@ end
 def array_with_two_elements
   my_array = [" "," "]
 end
+
+def first_element
+  
+end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+def first_element_with_array_methods
+
+end
+
+def last_element_with_array_methods(arr)
+  arr.pop
+  end
+
+def length_of_array(arr)
+arr.length   
+end
